4400b44e4f5c6feb700744b8186097fe30322ae6
anuj-softech <179094956+anuj-softech@users.noreply.github.com>
2024-09-27 00:38:11 +0530

    added wifi checking dialog

--- app/src/main/java/com/manit/hostel/assist/students/MainActivity.java
+++ app/src/main/java/com/manit/hostel/assist/students/MainActivity.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.graphics.Bitmap;
 import android.net.wifi.ScanResult;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.webkit.CookieManager;
@@ -11,9 +12,11 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.onesignal.Continue;
@@ -26,8 +29,8 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String URL = "http://4.186.57.254/index.php";
-    private String HOSTEL_WIFI_MAC_ADDRESS = "00:00:00:00:00:00";
+    public static final String URL = "http://4.186.57.254/for-app";
+    private String HOSTEL_WIFI_MAC_ADDRESS = "80:c5:f2:7b:ff:29";
     private boolean allowInVicinity = true;
     private boolean isPresentAtGate = false;
     private WebView webview;
@@ -41,10 +44,45 @@ public class MainActivity extends AppCompatActivity {
         splash = findViewById(R.id.splash);
         requestPermissions();
         Log.d(MainActivity.class.getSimpleName(), "Wifi Name list : " + new WifiScanner(this).getWifiList(this).toString());
-        checkForWifi();
-        loadWebview();
+        showWifiCheckDialog();
         setupOneSignal();
     }
+
+    private void showWifiCheckDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(R.layout.dialog_check_wifi);
+        AlertDialog dialog = builder.create();
+        dialog.setTitle("Checking WIFI of MANIT");
+        dialog.setCancelable(false);
+        dialog.show();
+
+        // Schedule a task to scan Wi-Fi every 5 seconds
+        final Handler handler = new Handler();
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                dialog.findViewById(R.id.wifi_progress).setVisibility(View.VISIBLE);
+                dialog.findViewById(R.id.success_icon).setVisibility(View.GONE);
+                ((TextView)dialog.findViewById(R.id.wifi_status)).setText("Scanning Hostel Wifi");
+                checkForWifi();  // Function to scan Wi-Fi
+                if(!isPresentAtGate){
+                    handler.postDelayed(this, 3000); // Repeat every 5 seconds
+                }else{
+                    ((TextView)dialog.findViewById(R.id.wifi_status)).setText("Found Hostel Wifi");
+                    dialog.findViewById(R.id.success_icon).setVisibility(View.VISIBLE);
+                    dialog.findViewById(R.id.success_icon).animate().alpha(1).setDuration(300).start();
+
+                    dialog.findViewById(R.id.wifi_progress).setVisibility(View.GONE);
+                    dialog.findViewById(R.id.wifi_progress).postDelayed(()->{
+                        loadWebview();
+                        dialog.dismiss();
+                    }, 1000);
+                }
+            }
+        };
+        handler.post(runnable);
+    }
+
 
     private void setupOneSignal() {
         String externalId = "241100112233"; // You will supply the external id to the OneSignal SDK
@@ -94,7 +132,7 @@ public class MainActivity extends AppCompatActivity {
                 String ssid = result.SSID;  // Network name
                 String bssid = result.BSSID; // MAC address of the network
                 Log.d("WiFi", "SSID: " + ssid + ", BSSID (MAC): " + bssid);
-                if (result.BSSID == HOSTEL_WIFI_MAC_ADDRESS) {
+                if (result.BSSID.equals(HOSTEL_WIFI_MAC_ADDRESS)) {
                     isPresentAtGate = true;
                 }
             }
